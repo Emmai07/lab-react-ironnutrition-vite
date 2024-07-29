ab4b24958448583a2199fa9fc0450c6f83289285
Emmai07 <171985268+Emmai07@users.noreply.github.com>
2024-07-30 02:57:59 +0500

    done
    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import { useState } from "react";
+
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+  const handleAdd = (newFood) => {
+    setFoods([...foods, newFood]);
+  };
+
+  const handleDelete = (foodName) => {
+    setFoods(foods.filter(food => food.name !== foodName));
+  };
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+
+      <AddFoodForm onAdd={handleAdd} />
+      {foods.map((food, index) => (
+        <FoodBox key={index} food={food} onDelete={handleDelete} />
+      ))}
+
+
     </div>
   );
 }
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,49 @@
-// Your code here
+import { useState } from 'react';
+
+function AddFoodForm({ onAdd }) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState('');
+  const [servings, setServings] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAdd({ name, image, calories: parseInt(calories), servings: parseInt(servings) });
+    setName('');
+    setImage('');
+    setCalories('');
+    setServings('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input name="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <br />
+        <br />
+      </label>
+      <label>
+        Image:
+        <input name="image" type="text" value={image} onChange={(e) => setImage(e.target.value)} />
+        <br />
+        <br />
+      </label>
+      <label>
+        Calories:
+        <input name="calories" type="number" value={calories} onChange={(e) => setCalories(e.target.value)} />
+        <br />
+        <br />
+      </label>
+      <label>
+        Servings:
+        <input name="servings" type="number" value={servings} onChange={(e) => setServings(e.target.value)} />
+      </label>
+      <br />
+      <br />
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;
